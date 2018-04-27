333f9bcaf21f2483f764313d53bb9a49621343e1
Refs <lhx1010339808@outlook.com>
2018-04-27 08:22:19 +0800

    .

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/observable';
+import { RequestOptionsArgs, Headers } from '@angular/http';
+
 
 /*
   Generated class for the AuthServiceProvider provider.
@@ -16,10 +19,16 @@ export class AuthServiceProvider {
     console.log('Hello AuthServiceProvider Provider');
   }
 
-  postData ( credentials, type ,options) {
-    return new Promise((resolve, reject ) => {
-      this.http.post(this.apiUrl + type , credentials, options)
-    })
+  //  此处我们应该去引入两个类型，一个是请求从服务器返回的类型用在 this.http.post<> 的尖括号里面， 一个处理过之后的数据类型，放在userLogin 返回的Observable<> 类型的尖括号里面；
+  // 除此之外，用户登陆与注册的时候，填写的列表， 也应有其对象类型；
+
+  // 现在的问题在于，若出现错误，则会出现类型不一致的情况；
+
+  userLogin( userData:any ): Observable<any> {
+
+    let requestOptions: RequestOptionsArgs
+
+    return this.http.post(this.apiUrl, userData)
   }
 
 }
